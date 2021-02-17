2d08862be5c50da6606210e795d185fe55e07c01
Andrea Bedini <andrea@andreabedini.com>
2021-02-17 16:23:27 +0800

    WIP

---
+++ cbits/bindings.c
@@ -0,0 +1,286 @@
+#include <wiredtiger.h>
+
+/*
+ * Cursor
+ */
+
+const char *cursor_uri(WT_CURSOR *cursor) {
+  return cursor->uri;
+}
+
+const char *cursor_key_format(WT_CURSOR *cursor) {
+  return cursor->key_format;
+}
+
+const char *cursor_value_format(WT_CURSOR *cursor) {
+  return cursor->value_format;
+}
+
+int cursor_get_key1(WT_CURSOR *cursor, char **key) {
+  return cursor->get_key(cursor, key);
+}
+
+int cursor_get_value1(WT_CURSOR *cursor, char **value) {
+  return cursor->get_value(cursor, value);
+}
+
+void cursor_set_key1(WT_CURSOR *cursor, char *key) {
+  cursor->set_key(cursor, key);
+}
+
+void cursor_set_value1(WT_CURSOR *cursor, char *value) {
+  cursor->set_value(cursor, value);
+}
+
+int cursor_compare(WT_CURSOR *cursor, WT_CURSOR *other, int *comparep) {
+  return cursor->compare(cursor, other, comparep);
+}
+
+int cursor_equals(WT_CURSOR *cursor, WT_CURSOR *other, int *equalp) {
+  return cursor->equals(cursor, other, equalp);
+}
+
+int cursor_next(WT_CURSOR *cursor) {
+  return cursor->next(cursor);
+}
+
+int cursor_prev(WT_CURSOR *cursor) {
+  return cursor->prev(cursor);
+}
+
+int cursor_reset(WT_CURSOR *cursor) {
+  return cursor->reset(cursor);
+}
+
+int cursor_search(WT_CURSOR *cursor) {
+  return cursor->search(cursor);
+}
+
+int cursor_search_near(WT_CURSOR *cursor, int *exactp) {
+  return cursor->search_near(cursor, exactp);
+}
+
+int cursor_insert(WT_CURSOR *cursor) {
+  return cursor->insert(cursor);
+}
+
+int cursor_modify(WT_CURSOR *cursor, WT_MODIFY *entries, int nentries) {
+  return cursor->modify(cursor, entries, nentries);
+}
+
+int cursor_update(WT_CURSOR *cursor) {
+  return cursor->update(cursor);
+}
+
+int cursor_remove(WT_CURSOR *cursor) {
+  return cursor->remove(cursor);
+}
+
+int cursor_reserve(WT_CURSOR *cursor) {
+  return cursor->reserve(cursor);
+}
+
+int cursor_close(WT_CURSOR *cursor) {
+  return cursor->close(cursor);
+}
+
+int cursor_reconfigure(WT_CURSOR *cursor, const char *config) {
+  return cursor->reconfigure(cursor, config);
+}
+
+int cursor_cache(WT_CURSOR *cursor) {
+  return cursor->cache(cursor);
+}
+
+int cursor_reopen(WT_CURSOR *cursor, bool check_only) {
+  return cursor->reopen(cursor, check_only);
+}
+
+/*
+ * Session
+ */
+
+int session_close(WT_SESSION *session, const char *config) {
+  return session->close(session, config);
+}
+
+int session_reconfigure(WT_SESSION *session, const char *config) {
+  return session->reconfigure(session, config);
+}
+
+const char* session_strerror(WT_SESSION *session, int error) {
+  return session->strerror(session, error);
+}
+
+int session_open_cursor(WT_SESSION *session, const char *uri,
+    WT_CURSOR *to_dup, const char *config, WT_CURSOR **cursorp) {
+  return session->open_cursor(session, uri, to_dup, config, cursorp);
+}
+
+int session_alter(WT_SESSION *session, const char *name, const char *config) {
+  return session->alter(session, name, config);
+}
+
+int session_create(WT_SESSION *session, const char *name, const char *config) {
+  return session->create(session, name, config);
+}
+
+int session_import(WT_SESSION *session, const char *name, const char *config) {
+  return session->import(session, name, config);
+}
+
+int session_drop(WT_SESSION *session, const char *name, const char *config) {
+  return session->drop(session, name, config);
+}
+
+int session_join(WT_SESSION *session, WT_CURSOR *join_cursor, WT_CURSOR *ref_cursor, const char *config) {
+  return session->join(session, join_cursor, ref_cursor, config);
+}
+
+/* int session_log_flush(WT_SESSION *session, const char *format, ...) */
+
+int session_rebalance(WT_SESSION *session, const char *uri, const char *config) {
+  return session->rebalance(session, uri, config);
+}
+
+int session_rename(WT_SESSION *session, const char *uri, const char *newuri, const char *config) {
+  return session->rename(session, uri, newuri, config);
+}
+
+int session_reset(WT_SESSION *session) {
+  return session->reset(session);
+}
+
+int session_salvage(WT_SESSION *session, const char *name, const char *config) {
+  return session->salvage(session, name, config);
+}
+
+int session_truncate(WT_SESSION *session, const char *name, WT_CURSOR* start, WT_CURSOR *stop, const char *config) {
+  return session->truncate(session, name, start, stop, config);
+}
+
+int session_upgrade(WT_SESSION *session, const char *name, const char *config) {
+  return session->upgrade(session, name, config);
+}
+
+int session_verify(WT_SESSION *session, const char *name, const char *config) {
+  return session->verify(session, name, config);
+}
+
+int session_begin_transaction(WT_SESSION *session, const char *config) {
+  return session->begin_transaction(session, config);
+}
+
+int session_commit_transaction(WT_SESSION *session, const char *config) {
+  return session->commit_transaction(session, config);
+}
+
+int session_prepare_transaction(WT_SESSION *session, const char *config) {
+  return session->prepare_transaction(session, config);
+}
+
+int session_rollback_transaction(WT_SESSION *session, const char *config) {
+  return session->rollback_transaction(session, config);
+}
+
+int session_timestamp_transaction(WT_SESSION *session, const char *config) {
+  return session->timestamp_transaction(session, config);
+}
+
+int session_query_timestamp(WT_SESSION *session, char *hex_timestamp, const char *config) {
+  return session->query_timestamp(session, hex_timestamp, config);
+}
+
+int session_checkpoint(WT_SESSION *session, const char *config) {
+  return session->checkpoint(session, config);
+}
+
+int session_snapshot(WT_SESSION *session, const char *config) {
+  return session->snapshot(session, config);
+}
+
+int session_transaction_pinned_range(WT_SESSION *session, uint64_t *range) {
+  return session->transaction_pinned_range(session, range);
+}
+
+int session_transaction_sync(WT_SESSION *session, const char *config) {
+  return session->transaction_sync(session, config);
+}
+
+/*
+ * Connection
+ */
+
+int connection_async_flush(WT_CONNECTION *connection) {
+  return connection->async_flush(connection);
+}
+
+// connection_async_new_op
+
+int connection_close(WT_CONNECTION *connection, const char *config) {
+  return connection->close(connection, config);
+}
+
+int connection_debug_info(WT_CONNECTION *connection, const char *config) {
+  return connection->debug_info(connection, config);
+}
+
+int connection_reconfigure(WT_CONNECTION *connection, const char *config) {
+  return connection->reconfigure(connection, config);
+}
+
+const char *connection_get_home(WT_CONNECTION *connection) {
+  return connection->get_home(connection);
+}
+
+int connection_configure_method(WT_CONNECTION *connection, const char *method, const char *uri, const char *config, const char *type, const char *check) {
+  return connection->configure_method(connection, method, uri, config, type, check);
+}
+
+int connection_is_new(WT_CONNECTION *connection) {
+  return connection->is_new(connection);
+}
+
+int connection_open_session(WT_CONNECTION *connection, WT_EVENT_HANDLER *event_handler, const char *config, WT_SESSION **sessionp) {
+  return connection->open_session(connection, event_handler, config, sessionp);
+}
+
+int connection_query_timestamp(WT_CONNECTION *connection, char *hex_timestamp, const char *config) {
+  return connection->query_timestamp(connection, hex_timestamp, config);
+}
+
+int connection_set_timestamp(WT_CONNECTION *connection, const char *config) {
+  return connection->set_timestamp(connection, config);
+}
+
+int connection_rollback_to_stable(WT_CONNECTION *connection, const char *config) {
+  return connection->rollback_to_stable(connection, config);
+}
+
+int connection_load_extension(WT_CONNECTION *connection, const char *path, const char *config) {
+  return connection->load_extension(connection, path, config);
+}
+
+int connection_add_data_source(WT_CONNECTION *connection, const char *prefix, WT_DATA_SOURCE *data_source, const char *config) {
+  return connection->add_data_source(connection, prefix, data_source, config);
+}
+
+int connection_add_collator(WT_CONNECTION *connection, const char *name, WT_COLLATOR *collator, const char *config) {
+  return connection->add_collator(connection, name, collator, config);
+}
+
+int connection_add_compressor(WT_CONNECTION *connection, const char *name, WT_COMPRESSOR *compressor, const char *config) {
+  return connection->add_compressor(connection, name, compressor, config);
+}
+
+int connection_add_encryptor(WT_CONNECTION *connection, const char *name, WT_ENCRYPTOR *compressor, const char *config) {
+  return connection->add_encryptor(connection, name, compressor, config);
+}
+
+int connection_add_extractor(WT_CONNECTION *connection, const char *name, WT_EXTRACTOR *compressor, const char *config) {
+  return connection->add_extractor(connection, name, compressor, config);
+}
+
+int connection_set_file_system(WT_CONNECTION *connection, WT_FILE_SYSTEM *fs, const char *config) {
+  return connection->set_file_system(connection, fs, config);
+}
---
+++ src/Bindings.chs
@@ -0,0 +1,405 @@
+{-# LANGUAGE CPP #-}
+{-# LANGUAGE ForeignFunctionInterface #-}
+
+#include "bindings.c"
+
+module Bindings where
+
+import Foreign
+import Foreign.C.String
+import Foreign.C.Types
+
+
+{# pointer *WT_COLLATOR as Collator #}
+{# pointer *WT_COMPRESSOR as Compressor #}
+{# pointer *WT_CONNECTION as Connection #}
+{# pointer *WT_CURSOR as Cursor #}
+{# pointer *WT_DATA_SOURCE as DataSource #}
+{# pointer *WT_ENCRYPTOR as Encryptor #}
+{# pointer *WT_EVENT_HANDLER as EventHandler #}
+{# pointer *WT_EXTENSION_API as ExtensionAPI #}
+{# pointer *WT_EXTRACTOR as Extractor #}
+{# pointer *WT_FILE_HANDLE as FileHandle #}
+{# pointer *WT_FILE_SYSTEM as FileSystem #}
+{# pointer *WT_SESSION as Session #}
+
+--
+-- Cursor
+--
+
+{# fun unsafe cursor_uri as ^
+  { `Cursor'
+  } -> `String' #}
+
+{# fun unsafe cursor_key_format as ^
+  { `Cursor'
+  } -> `String' #}
+
+{# fun unsafe cursor_value_format as ^
+  { `Cursor'
+  } -> `String' #}
+
+--get_key1
+
+--get_value1
+
+{# fun unsafe cursor_set_key1 as ^
+  { `Cursor'
+  , `String'
+  } -> `()' #}
+
+{# fun unsafe cursor_set_value1 as ^
+  { `Cursor'
+  , `String'
+  } -> `()' #}
+
+{# fun unsafe cursor_compare as ^
+  { `Cursor'
+  , `Cursor'
+  , alloca- `Int' peekInt*
+  } -> `Int' #}
+
+{# fun unsafe cursor_equals as ^
+  { `Cursor'
+  , `Cursor'
+  , alloca- `Int' peekInt*
+  } -> `Int' #}
+
+{# fun unsafe cursor_next as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_prev as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_reset as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_search as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_search_near as ^
+  { `Cursor'
+  , alloca- `Int' peekInt*
+  } -> `Int' #}
+
+{# fun unsafe cursor_insert as ^
+  { `Cursor'
+  } -> `Int' #}
+
+-- cursor_modify
+
+{# fun unsafe cursor_update as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_remove as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_reserve as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_close as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_reconfigure as ^
+  { `Cursor'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe cursor_cache as ^
+  { `Cursor'
+  } -> `Int' #}
+
+{# fun unsafe cursor_reopen as ^
+  { `Cursor'
+  , `Bool'
+  } -> `Int' #}
+
+--
+-- Session
+--
+
+{# fun unsafe session_close as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_reconfigure as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_strerror as ^
+  { `Session'
+  , `Int'
+  } -> `String' #}
+
+{# fun unsafe session_open_cursor as ^
+  { `Session'
+  , `String'
+  , optionally `Maybe Cursor'
+  , `String'
+  , alloca- `Cursor' peek*
+  } -> `Int' #}
+
+{# fun unsafe session_alter as ^
+  { `Session'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_create as ^
+  { `Session'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_import as ^
+  { `Session'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_drop as ^
+  { `Session'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_join as ^
+  { `Session'
+  , `Cursor'
+  , `Cursor'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_rebalance as ^
+  { `Session'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_rename as ^
+  { `Session'
+  , `String'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_reset as ^
+  { `Session'
+  } -> `Int' #}
+
+{# fun unsafe session_salvage as ^
+  { `Session'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_truncate as ^
+  { `Session'
+  , `String'
+  , `Cursor'
+  , `Cursor'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_upgrade as ^
+  { `Session'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_verify as ^
+  { `Session'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_begin_transaction as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_commit_transaction as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_prepare_transaction as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_rollback_transaction as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_timestamp_transaction as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_query_timestamp as ^
+  { `Session'
+  , `String'
+  , alloca- `String' peekCString*
+  } -> `Int' #}
+
+{# fun unsafe session_checkpoint as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_snapshot as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe session_transaction_pinned_range as ^
+  { `Session'
+  , `Int'
+  } -> `Int' #}
+
+{# fun unsafe session_transaction_sync as ^
+  { `Session'
+  , `String'
+  } -> `Int' #}
+
+--
+-- Connection
+--
+
+{# fun unsafe connection_async_flush as ^
+  { `Connection'
+  } -> `Int' #}
+
+{# fun unsafe connection_close as ^
+  { `Connection'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_debug_info as ^
+  { `Connection'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_reconfigure as ^
+  { `Connection'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_get_home as ^
+  { `Connection'
+  } -> `String' #}
+
+{# fun unsafe connection_configure_method as ^
+  { `Connection'
+  , `String'
+  , `String'
+  , `String'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_is_new as ^
+  { `Connection'
+  } -> `Bool' #}
+
+{# fun unsafe connection_open_session as ^
+  { `Connection'
+  , optionally `Maybe EventHandler'
+  , `String'
+  , alloca- `Session' peek*
+  } -> `Int' #}
+
+{# fun unsafe connection_query_timestamp as ^
+  { `Connection'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_set_timestamp as ^
+  { `Connection'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_rollback_to_stable as ^
+  { `Connection'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_load_extension as ^
+  { `Connection'
+  , `String'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_add_data_source as ^
+  { `Connection'
+  , `String'
+  , `DataSource'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_add_collator as ^
+  { `Connection'
+  , `String'
+  , `Collator'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_add_compressor as ^
+  { `Connection'
+  , `String'
+  , `Compressor'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_add_encryptor as ^
+  { `Connection'
+  , `String'
+  , `Encryptor'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_add_extractor as ^
+  { `Connection'
+  , `String'
+  , `Extractor'
+  , `String'
+  } -> `Int' #}
+
+{# fun unsafe connection_set_file_system as ^
+  { `Connection'
+  , `FileSystem'
+  , `String'
+  } -> `Int' #}
+
+--
+--
+--
+
+{# fun unsafe wiredtiger_open as open
+  { `String'
+  , optionally `Maybe EventHandler'
+  , `String'
+  , alloca- `Connection' peek*
+  } -> `Int' #}
+
+--
+-- Utilities
+--
+
+optionally = maybe nullPtr id
+
+peekInt p = fromIntegral <$> peek p
+
